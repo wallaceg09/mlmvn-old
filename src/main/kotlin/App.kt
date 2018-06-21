f1ecc3c9ecfb7afeb6c7fa663c49b2b0f6d3cc4b
Glen Wallace <wallaceglenw@gmail.com>
2018-06-21 12:11:29 -0500

    Add Complex number dependencies
    Signed-off-by: Glen Wallace <wallaceglenw@gmail.com>

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,5 +1,12 @@
 package com.wallace.intelligence.mlmvn
 
+import com.wallace.math.complex.times
+import org.apache.commons.math3.complex.Complex
+
 fun main(args: Array<String>) {
     println("Hello, World!")
+    val c1 = Complex(1.0, 2.0)
+    val c2 = Complex(2.0, 3.0)
+
+    println(c1 * c2)
 }
